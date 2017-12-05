357b90ee838d09d09e3f9c990bb4e635de669b31
carlosmarze <c_arze@hotmail.com>
2017-12-05 11:07:39 -0300

    Mi IP en 000web
    http://c-arze.000webhostapp.com/MiWeb/MiIP.php

--- MiIP.php
+++ MiIP.php
@@ -2,52 +2,19 @@
 <BODY>
 
 Mi IP:
+<br>
 
 Comienzo PHP
-
+<br>
 <?php 
  //Gets the IP address
  $ip = getenv("REMOTE_ADDR") ; 
- Echo "Your IP is " . $ip; 
+ Echo "La IP es: " . $ip; 
  ?> 
- 
- Fin PHP
-
-
-<script type="text/javascript"> var userip; </script> 
-<script type="text/javascript" 
-$.get("http://ipinfo.io", function(response) {
-    alert(response.ip);
-}, "jsonp");>
-</script>
-<script type="text/javascript">document.write("Your IP address is :", userip); </script> 
-
-<script type="text/javascript" 
-$(document).ready(function () {
-    $.getJSON("http://jsonip.com/?callback=?", function (data) {
-        console.log(data);
-        alert(data.ip);
-    });
-});>
-
-</script>
-
-WTI:
-
-<script type="text/javascript"
-	src="http://www.oil-price.net/TABLE2/gen.php?lang=en">
-</script>
-<noscript> To get the WTI <a href="http://www.oil-price.net/dashboard.php?lang=en#TABLE2">oil price</a>, please enable Javascript.
-</noscript>
-'
-'
-BRENT:
-
-<script type="text/javascript"
-	src="http://www.oil-price.net/widgets/brent_crude_price_large/gen.php?lang=en">
-</script>
-<noscript> To get the BRENT <a href="http://www.oil-price.net/dashboard.php?lang=en#brent_crude_price_large">oil price</a>, please enable Javascript.
-</noscript>
+ <br>
+Fin PHP
+<br>
+<br>
 
 
 </BODY>
